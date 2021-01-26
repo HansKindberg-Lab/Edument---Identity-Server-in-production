1bf2934d8e40fc629b3d0191e128556c95b53843
HansKindberg <hans.kindberg@gmail.com>
2021-01-26 11:24:10 +0100

    Main exercise 4

--- IdentityService/Startup.cs
+++ IdentityService/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServerHost.Quickstart.UI;
+using IdentityServerInMem;
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,57 +29,55 @@ namespace IdentityService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-	        services.AddHsts(opts =>
+            services.AddHsts(opts =>
 	        {
 		        opts.IncludeSubDomains = true;
 		        opts.MaxAge = TimeSpan.FromSeconds(15768000);
 	        });
+
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer(options =>
+	            {
+
+	            }).AddTestUsers(TestUsers.Users)
+	            .AddInMemoryIdentityResources(Config.IdentityResources)
+	            .AddInMemoryApiScopes(Config.ApiScopes)
+	            .AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (!env.IsProduction())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-	            app.UseHsts();
-            }
+	        if (!env.IsProduction())
+	        {
+		        app.UseDeveloperExceptionPage();
+	        }
+	        else
+	        {
+		        app.UseHsts();
+	        }
 
-            app.UseHttpsRedirection();
+	        app.UseHttpsRedirection();
+	        app.UseSecurityHeaders();
 
-            app.UseSecurityHeaders();
+	        app.UseRequestLocalization(new RequestLocalizationOptions().SetDefaultCulture("se-SE"));
 
-            app.UseRouting();
+	        app.UseStaticFiles();
 
-            app.UseRequestLocalization(
-	            new RequestLocalizationOptions()
-		            .SetDefaultCulture("se-SE"));
+	        app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    string build = "Debug build";
-                    if (Settings.IsReleaseBuild)
-                    {
-                        build = "Release build";
-                    }
+	        app.UseIdentityServer();
 
-                    //Print out the first 8 characters of the GitHub SHA when deploying to production
-                    //Should of course be a bit more hidden in real life, perhaps as a HTML comment?
-                    var gitHubSha = _configuration["GITHUB:SHA"] ?? "";
-                    if (gitHubSha.Length > 8)
-                    {
-                        gitHubSha = " " + gitHubSha.Substring(0, 8);
-                    }
+	        app.UseAuthorization();
 
-                    //In real life this Should of course be a bit more hidden, perhaps as a HTML comment?
-                    await context.Response.WriteAsync($"Hello Identity Service!, Deployed {Infrastructure.Settings.StartupTime} ({env.EnvironmentName}, {build}{gitHubSha})");
-                });
-            });
+	        app.UseEndpoints(endpoints =>
+	        {
+		        endpoints.MapDefaultControllerRoute();
+	        });
         }
     }
 }
